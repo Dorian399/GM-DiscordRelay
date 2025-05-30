6ea29c3e2eb3fac19238425659e0dcd7c0605e7f
Dorian399 <92229312+Dorian399@users.noreply.github.com>
2025-05-30 13:15:48 +0200

    Add debug logs, and begin work on server status message

--- index.js
+++ index.js
@@ -77,8 +77,13 @@ async function run() {
 	console.log('Log capture sucessfully started.');
 }
 
-// Converts string to Base64.
+function debugLog(str) {
+	if(config.ShowDebugLogs){
+		console.log(str);
+	}
+}
 
+// Converts string to Base64.
 function utf8ToBase64(str) {
 	const utf8Bytes = new TextEncoder().encode(str);
 	const binaryStr = String.fromCharCode(...utf8Bytes);
@@ -102,15 +107,18 @@ function generateHash(str) {
 
 // Sends a rcon command to the specified server. Returns the command's response.
 async function sendRCON(ip,port,pass,command) {
+	debugLog('\n[RCON] Sending RCON request');
 	return new Promise((resolve,reject) => {
 		const conn = new Rcon(ip,port,pass);
 		let resolved = false;
 		let result = '';
 		let timeoutID;
 		conn.on('auth',() => {
+			debugLog('[RCON] Auth successful, sending command.');
 			conn.send(command);
 		});
 		conn.on('response',(str) => {
+			debugLog('[RCON] Received response/response chunk.');
 			if(timeoutID)
 				clearTimeout(timeoutID);
 			result += str;
@@ -118,6 +126,7 @@ async function sendRCON(ip,port,pass,command) {
 				if(!resolved){
 					resolved=true;
 					resolve(result);
+					debugLog('[RCON] Result gathered, closing connection manually.');
 					conn.disconnect();
 				}
 			}, 500);	
@@ -125,10 +134,12 @@ async function sendRCON(ip,port,pass,command) {
 		conn.on('error',(err) => {
 			if(!resolved){
 				resolved=true;
+				debugLog('[RCON] Error: '+err.toString()+'.');
 				resolve(err.toString());
 			}
 		});
 		conn.on('end',() => {
+			debugLog('[RCON] Connection ended.\n');
 			if(!resolved){
 				resolved=true;
 				resolve(result);
@@ -142,10 +153,17 @@ async function sendRCON(ip,port,pass,command) {
 // Fetches ancd caches the avatar url from a steam profile.
 let avatarCache = {};
 async function fetchAvatarUrl(steamid) {
-	if(!config.SteamAPIKey || config.SteamAPIKey.length <20)
+	debugLog('\n[AVATAR] Avatar fetch started.');
+	if(!config.SteamAPIKey || config.SteamAPIKey.length <20){
+		debugLog('[AVATAR] Invalid or missing API key.\n');
 		return;
-	if(avatarCache[steamid])
+	}
+	if(avatarCache[steamid]){
+		debugLog('[AVATAR] Accessing URL from cache.');
+		debugLog('[AVATAR] Avatar fetched successfully.\n');
 		return avatarCache[steamid];
+	}
+	debugLog('[AVATAR] Requesting from API.');
 	const steamidsplit = steamid.split(':');
 	const X = parseInt(steamidsplit[0].replace('STEAM_', ''));
 	const Y = parseInt(steamidsplit[1]);
@@ -155,19 +173,24 @@ async function fetchAvatarUrl(steamid) {
 	let res = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.SteamAPIKey}&steamids=${steamid64}`);
 	let data = await res.json();
 	avatarCache[steamid]=data.response.players[0].avatarfull;
+	debugLog('[AVATAR] Avatar fetched successfully.\n');
 	return avatarCache[steamid];
 }
 
 
 // Assigns an existing webhook or creates a new one if it doesn't exists.
 async function fetchChannelWebhooks(channel) {
+	debugLog('\n[WEBHOOK] Retrieving webhook for channel: '+channel.name+'.');
 	try {
 		const hooks = await channel.fetchWebhooks();
 		let webhook = hooks.find(hook => hook.name === 'srcds_chat_relay');
 		if(!webhook){
+			debugLog('[WEBHOOK] Webhook not found, creating a new one.\n');
 			webhook = await channel.createWebhook({
 				name: 'srcds_chat_relay'
 			});
+		}else{
+			debugLog('[WEBHOOK] Webhook found.\n');
 		}
 		return webhook;
 	} catch (error) {
@@ -175,9 +198,30 @@ async function fetchChannelWebhooks(channel) {
 	}
 }
 
+// Assigns an existing webhook or creates a new one if it doesn't exists.
+async function fetchStatusChannel(channel,botid) {
+	debugLog('\n[STATUS] Retrieving existing status message: '+channel.name+'.');
+	try {
+		const messages = await channel.messages.fetch({ limit: 100 });
+		let statusMessage = messages.find(message => message.author.id == botid);
+		if(!statusMessage){
+			debugLog('[STATUS] Status message not found, creating a new one.\n');
+			statusMessage = await channel.send('Status Message.');
+		}else{
+			debugLog('[STATUS] Status message found.\n');
+		}
+		return statusMessage;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
 // Set up webhooks for each server's channel.
 let relayWebhooks = {};
+// Set up status message.
+let statusMessage;
 client.once(Events.ClientReady, async readyClient => {
+	// Prepare webhooks.
 	for(var key in config.Servers){
 		const channel = client.channels.cache.get(config.Servers[key].relaychannel);
 		if(!channel)
@@ -185,6 +229,13 @@ client.once(Events.ClientReady, async readyClient => {
 		const webhook = await fetchChannelWebhooks(channel);
 		relayWebhooks[config.Servers[key].relaychannel] = webhook;
 	}
+	
+	//Prepare status message.
+	if(config.StatusChannelID.length >= 16) {
+		const channel = client.channels.cache.get(config.StatusChannelID);
+		statusMessage = await fetchStatusChannel(channel,readyClient.user.id);
+	}
+	
 	console.log(`Bot started. Logged in as ${readyClient.user.tag}`);
 	run().catch(console.log);
 });
@@ -192,8 +243,11 @@ client.once(Events.ClientReady, async readyClient => {
 // Capture and relay logs.
 receiver.on('log', async (log) => {
 	const ip_and_port = log.receivedFrom.address + ':' + log.receivedFrom.port;
-	if(!serverChannelIDs[ip_and_port])
+	debugLog('\n[LOG] Received log from: '+ip_and_port+'.');
+	if(!serverChannelIDs[ip_and_port]){
+		debugLog('[LOG] No channelID found, skipping.\n');
 		return;
+	}
 	const channelID = serverChannelIDs[ip_and_port];
 	const message = log.payload;
 	const matched_message = message.match(/<[0-9]+><STEAM_[0-5]:[01]:\d+><Team>" (say|say_team) "/);
@@ -203,13 +257,19 @@ receiver.on('log', async (log) => {
 			return;
 		const regex = /^(?:\d{2}\/\d{2}\/\d{4} - \d{2}:\d{2}:\d{2}: )(Lua Error:\s*\n\[ERROR\][\s\S]*)$/m;
 		const matched_luaerror = message.match(regex);
-		if(!matched_luaerror)
+		if(!matched_luaerror){
+			debugLog('[LOG] Log does not match any of the criteria, skipping.\n');
 			return;
+		}
+		debugLog('[LOG] Log is a Lua error.');
 		const channel = client.channels.cache.get(channelID);
 		const webhook = relayWebhooks[channelID];
-		if(!webhook)
+		if(!webhook){
+			debugLog('[LOG] Webhook not found or invalid, skipping.\n');
 			return;
+		}
 		try{
+			debugLog('[LOG] Sending Lua error to the webhook.\n');
 			webhook.send({
 				content: matched_luaerror[1],
 				username: 'Lua Error',
@@ -218,33 +278,44 @@ receiver.on('log', async (log) => {
 			console.error(error);
 		}
 		return;
+	}else{
+		debugLog('[LOG] Log is a chatlog.');
 	}
 	// Chat messages.
 	const channel = client.channels.cache.get(channelID);
 	const steamid = message.match(/STEAM_[0-5]:[01]:\d+/)[0].replace('<','').replace('>','');
 	const username = message.match(/^.*?"(.*?)<\d+><STEAM_0:[01]:\d+><.*?>"/)[1];
 	const content_match = message.match(/^.*?"[^"]*<\d+><STEAM_0:[01]:\d+><[^>]+>"\s+say(?:_team)?\s+"(.*)"/);
-	if(!content_match || !content_match[1])
+	if(!content_match || !content_match[1]){
+		debugLog('[LOG] Chatlog invalid, skipping.\n');
 		return;
+	}
 	const content = content_match[1].replace('@','@ ');
 	if(channel){
 		const avatar = await fetchAvatarUrl(steamid);
 		const webhook = relayWebhooks[channelID];
-		if(!webhook)
+		if(!webhook){
+			debugLog('[LOG] Webhook not found or invalid, skipping.\n');
 			return;
+		}
 		try{
+			debugLog('[LOG] Sending chatlog to webhook.');
 			webhook.send({
 				content: content,
 				username: username,
 				avatarURL: avatar,
 			});
 		}catch(error){
+			debugLog('[LOG] Sending failed, retrying.');
 			webhook.send({
 				content: ' '+content,
 				username: username,
 				avatarURL: avatar,
 			});
 		}
+		debugLog('[LOG] Chatlog sent.\n');
+	}else{
+		debugLog('[LOG] Invalid channelID, skipping.\n');
 	}
 });
 
@@ -252,7 +323,11 @@ receiver.on('log', async (log) => {
 
 // Relays messages and detect commands.
 client.on(Events.MessageCreate, async message => {
+	if(!message.author.bot)
+		debugLog('\n[MESSAGE] Received message from: '+message.author.globalName+'.');
 	if(!serverChannelIDs[message.channelId] || !serverChannelIDs[message.channelId].ip || !serverChannelIDs[message.channelId].port || message.author.bot){
+		if(!message.author.bot)
+			debugLog('[MESSAGE] Message is not a part of the relay channels, skipping.\n');
 		return;
 	}
 	const ip = serverChannelIDs[message.channelId].ip;
@@ -264,11 +339,15 @@ client.on(Events.MessageCreate, async message => {
 	
 	for(var i in rconAliases){
 		if(content.startsWith(rconAliases[i])){
-			if(!usersWhitelist[author])
+			debugLog('[MESSAGE] Command detected.\n');
+			if(!usersWhitelist[author]){
+				debugLog('[MESSAGE] User does not have permission to execute commands, skipping.\n');
 				return;
+			}
 			const command = content.substring(rconAliases[i].length, message.length).trim().replace('"',"'");
 			const reply = await message.reply('Executing command : '+command);
 			const result = await sendRCON(ip,port,pass,command);
+			debugLog('[MESSAGE] Command executed.');
 			if(!result || result.length <=0){
 				reply.edit('Command executed but returned no results.');
 				return;
@@ -278,10 +357,13 @@ client.on(Events.MessageCreate, async message => {
 		}
 	}
 	
-	if(!ip || !port || !username || !author || !message || !content)
+	if(!ip || !port || !username || !author || !message || !content){
+		debugLog('[MESSAGE] Invalid value detected (Either missing sender data, or invalid ip/port) ,skipping.\n');
 		return;
+	}
 	
 	if(content.length > config.MaxMessageLength){
+		debugLog('[MESSAGE] Message is over the max message length, trimming message.');
 		content = content.substring(0,config.MaxMessageLength);
 		message.react('✂');
 	}
@@ -291,18 +373,27 @@ client.on(Events.MessageCreate, async message => {
 	const messageHash = generateHash(message.id);
 	const startSize = relayCommand.length+2+usernameb64.length;
 	if(contentb64.length <= 500 - startSize){  // For requests less 500 bytes in total, no chunking.
+		debugLog('[MESSAGE] Message fits into a single request.');
 		const command = relayCommand+' '+usernameb64+' '+contentb64;
+		debugLog('[MESSAGE] Sending message to the server.');
 		const result = await sendRCON(ip,port,pass,command);
 		if( result.length>0 && result.toLowerCase().includes('error') ){
+			debugLog('[MESSAGE] Server errored or has not responded.\n');
 			message.react('❌');
+			return;
 		}
+		debugLog('[MESSAGE] Message sucessfully sent to server.');
 	}else{ // For requests over 500 bytes in total, chunking.
+		debugLog('[MESSAGE] Message does not fit into a single request, chunking.');
 		const chunkSize = 500 - (relayCommand.length+2+messageHash.length);
 		const requestsAmount = Math.ceil(contentb64.length / chunkSize);
 		// First request acts as a starting point with the username and an unique hash.
 		const firstRequest_command = relayCommand+' 0 '+requestsAmount+' '+messageHash+' '+usernameb64;
+		debugLog('[MESSAGE] Sending message chunks to the server.');
 		const firstRequest = await sendRCON(ip,port,pass,firstRequest_command);
+		debugLog('[MESSAGE] Chunk 0 sent.');
 		if( firstRequest.length>0 && firstRequest.toLowerCase().includes('error') ){
+			debugLog('[MESSAGE] Server errored or has not responded.\n');
 			message.react('❌');
 			return;
 		}
@@ -310,7 +401,9 @@ client.on(Events.MessageCreate, async message => {
 			const chunk = contentb64.substring((i-1)*chunkSize,i*chunkSize);
 			const command = relayCommand+' '+i+' '+messageHash+' '+chunk;
 			await sendRCON(ip,port,pass,command);
+			debugLog('[MESSAGE] Chunk '+i+' sent.');
 		}
+		debugLog('[MESSAGE] All chunks have been sent.\n');
 	}
 });
 
